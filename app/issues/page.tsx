c04f5203b58c3d8dffe98bcad85345c6c1f412b6
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2025-05-01 22:51:26 +0530

    Build the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Flex, Text, Button } from "@radix-ui/themes";
+import Link from "next/link";
+import { Button } from "@radix-ui/themes";
 
 const IssuesPage = () => {
   return (
-    <Flex direction="column" gap="2">
-      <Text>Hello from Radix Themes :)</Text>
-      <Button>Let&apos;s go</Button>
-    </Flex>
+    <div>
+      <Button>
+        <Link href={"/issues/new"}>New Issue</Link>
+      </Button>
+    </div>
   );
 };
 
