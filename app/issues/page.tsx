a86383fc25dd87d75e27412269c5eb03679b4702
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2025-05-01 22:29:53 +0530

    Set up Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Flex, Text, Button } from "@radix-ui/themes";
 
 const IssuesPage = () => {
-  return <div>Issue page</div>;
+  return (
+    <Flex direction="column" gap="2">
+      <Text>Hello from Radix Themes :)</Text>
+      <Button>Let&apos;s go</Button>
+    </Flex>
+  );
 };
 
 export default IssuesPage;
