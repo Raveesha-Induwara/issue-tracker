2f2b0e19d35cd4eb6103576511aa398f92c6eb88
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2025-05-04 21:49:54 +0530

    Refactor: organize import statements

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -5,16 +5,15 @@ import { useState } from "react";
 import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
-import Spinner from "@/app/components/Spinner";
 import { TextField, Button } from "@radix-ui/themes";
 import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import ErrorMessage from "@/app/components/ErrorMessage";
+import { ErrorMessage, Spinner } from "@/app/components";
 import { createIssueSchema } from "../../validationSchema";
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
-})
+});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,7 @@
 import prisma from "@/prisma/client";
-import Link from "../components/Link";
 import { Table } from "@radix-ui/themes";
 import IssueActions from "./IssueActions";
-import IssueStatusBadge from "../components/IssueStatusBadge";
+import {Link, IssueStatusBadge} from "@/app/components";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany({});
