f4f1cd1cbe102903d069ed476fb6434ed2e13f18
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2025-05-04 20:33:52 +0530

    Create issue details page

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,42 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import React from "react";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+const page = async ({ params: { id } }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(id) },
+  });
+
+  if (!issue) {
+    notFound();
+  }
+  return (
+    <div>
+      <Heading>{issue.title}</Heading>
+      <Flex className="my-5" gap={"5"}>
+        <IssueStatusBadge status={issue.status} />
+        <Text>
+          {issue.createdAt.toLocaleDateString("en-us", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          })}
+        </Text>
+      </Flex>
+      <Card className="prose ">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
+    </div>
+  );
+};
+
+export default page;
